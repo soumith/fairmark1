ef90006e38e909fd9726106e5d52b3eb256e1e57
soumith <soumith@fb.com>
2015-08-09 14:31:52 -0700

    run with luajit to remove color characters from output

--- imagenet/runall_osstorch.sh
+++ imagenet/runall_osstorch.sh
@@ -1,50 +1,50 @@
-th main.lua -netType alexnet -backend cudnn -nGPU 1
-th main.lua -netType alexnet -backend cudnn -nGPU 2
-#th main.lua -netType alexnet -backend fbcunn -nGPU 1
-#th main.lua -netType alexnet -backend fbcunn -nGPU 2
-#th main.lua -netType alexnet -backend cunn -nGPU 1
-#th main.lua -netType alexnet -backend cunn -nGPU 2
+luajit main.lua -netType alexnet -backend cudnn -nGPU 1
+luajit main.lua -netType alexnet -backend cudnn -nGPU 2
+#luajit main.lua -netType alexnet -backend fbcunn -nGPU 1
+#luajit main.lua -netType alexnet -backend fbcunn -nGPU 2
+#luajit main.lua -netType alexnet -backend cunn -nGPU 1
+#luajit main.lua -netType alexnet -backend cunn -nGPU 2
 
-th main.lua -netType alexnetowt -backend cudnn -nGPU 1
-th main.lua -netType alexnetowt -backend cudnn -nGPU 2
-th main.lua -netType alexnetowt -backend cudnn -nGPU 4
-th main.lua -netType alexnetowt -backend cudnn -nGPU 8
+luajit main.lua -netType alexnetowt -backend cudnn -nGPU 1
+luajit main.lua -netType alexnetowt -backend cudnn -nGPU 2
+luajit main.lua -netType alexnetowt -backend cudnn -nGPU 4
+luajit main.lua -netType alexnetowt -backend cudnn -nGPU 8
 
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 1
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 2
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 4
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 8
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 4 -batchSize 256
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 8 -batchSize 256
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 4 -batchSize 512
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 8 -batchSize 512
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 4 -batchSize 1024
-th main.lua -netType alexnetowtbn -backend cudnn -nGPU 8 -batchSize 1024
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 1
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 2
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 4
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 8
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 4 -batchSize 256
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 8 -batchSize 256
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 4 -batchSize 512
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 8 -batchSize 512
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 4 -batchSize 1024
+luajit main.lua -netType alexnetowtbn -backend cudnn -nGPU 8 -batchSize 1024
 
-th main.lua -netType overfeat -backend cudnn -nGPU 1
-th main.lua -netType overfeat -backend cudnn -nGPU 2
-th main.lua -netType overfeat -backend cudnn -nGPU 4
-th main.lua -netType overfeat -backend cudnn -nGPU 8
-th main.lua -netType overfeat -backend cudnn -nGPU 4 -batchSize 256
-th main.lua -netType overfeat -backend cudnn -nGPU 8 -batchSize 256
-th main.lua -netType overfeat -backend cudnn -nGPU 4 -batchSize 512
-th main.lua -netType overfeat -backend cudnn -nGPU 8 -batchSize 512
+luajit main.lua -netType overfeat -backend cudnn -nGPU 1
+luajit main.lua -netType overfeat -backend cudnn -nGPU 2
+luajit main.lua -netType overfeat -backend cudnn -nGPU 4
+luajit main.lua -netType overfeat -backend cudnn -nGPU 8
+luajit main.lua -netType overfeat -backend cudnn -nGPU 4 -batchSize 256
+luajit main.lua -netType overfeat -backend cudnn -nGPU 8 -batchSize 256
+luajit main.lua -netType overfeat -backend cudnn -nGPU 4 -batchSize 512
+luajit main.lua -netType overfeat -backend cudnn -nGPU 8 -batchSize 512
 
 
-th main.lua -netType vgg -backend cudnn -nGPU 1
-th main.lua -netType vgg -backend cudnn -nGPU 2
-th main.lua -netType vgg -backend cudnn -nGPU 4
-th main.lua -netType vgg -backend cudnn -nGPU 8
-th main.lua -netType vgg -backend cudnn -nGPU 4 -batchSize 256
-th main.lua -netType vgg -backend cudnn -nGPU 8 -batchSize 256
-th main.lua -netType vgg -backend cudnn -nGPU 4 -batchSize 512
-th main.lua -netType vgg -backend cudnn -nGPU 8 -batchSize 512
+luajit main.lua -netType vgg -backend cudnn -nGPU 1
+luajit main.lua -netType vgg -backend cudnn -nGPU 2
+luajit main.lua -netType vgg -backend cudnn -nGPU 4
+luajit main.lua -netType vgg -backend cudnn -nGPU 8
+luajit main.lua -netType vgg -backend cudnn -nGPU 4 -batchSize 256
+luajit main.lua -netType vgg -backend cudnn -nGPU 8 -batchSize 256
+luajit main.lua -netType vgg -backend cudnn -nGPU 4 -batchSize 512
+luajit main.lua -netType vgg -backend cudnn -nGPU 8 -batchSize 512
 
-th main.lua -netType googlenet -backend cudnn -nGPU 1
-th main.lua -netType googlenet -backend cudnn -nGPU 2
-th main.lua -netType googlenet -backend cudnn -nGPU 4
-th main.lua -netType googlenet -backend cudnn -nGPU 8
-th main.lua -netType googlenet -backend cudnn -nGPU 4 -batchSize 256
-th main.lua -netType googlenet -backend cudnn -nGPU 8 -batchSize 256
-th main.lua -netType googlenet -backend cudnn -nGPU 4 -batchSize 512
-th main.lua -netType googlenet -backend cudnn -nGPU 8 -batchSize 512
+luajit main.lua -netType googlenet -backend cudnn -nGPU 1
+luajit main.lua -netType googlenet -backend cudnn -nGPU 2
+luajit main.lua -netType googlenet -backend cudnn -nGPU 4
+luajit main.lua -netType googlenet -backend cudnn -nGPU 8
+luajit main.lua -netType googlenet -backend cudnn -nGPU 4 -batchSize 256
+luajit main.lua -netType googlenet -backend cudnn -nGPU 8 -batchSize 256
+luajit main.lua -netType googlenet -backend cudnn -nGPU 4 -batchSize 512
+luajit main.lua -netType googlenet -backend cudnn -nGPU 8 -batchSize 512
